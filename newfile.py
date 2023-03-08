211fd5326f50785f666c6ede4e5dedf2f7dd766a
Keshav Agarwal <64908981+MasterK0927@users.noreply.github.com>
2023-03-08 19:48:36 +0530

    Added Sastanasha plugin

---
+++ newfile.py
@@ -0,0 +1,17 @@
+import asyncio
+
+@ultroid_cmd(pattern="sA")
+async def sastanasha(ult):
+	await ult.edit("Going to buy sasta nasha for You 'Ayushman Batttery'                                                $$$$$$$$$$$$$$$$$$$$$$$$")
+	await asyncio.sleep(2)
+	await ult.edit("Sasta Nasha Bought,   Bringing it to you.                                                  🚶🚶‍♂🚶‍♂🚶‍♂🚶‍♂🚶‍♂🚶‍♂🚶‍♂🚶🚶‍♂🚶‍♂🚶‍♂🚶‍♂🚶‍♂🚶‍♂🚶‍♂🚶🚶‍♂🚶‍♂🚶‍♂🚶‍♂🚶‍♂🚶‍♂🚶‍♂🚶🚶‍♂🚶‍♂🚶‍♂🚶‍♂🚶‍♂🚶‍♂🚶‍♂🚶🚶‍♂🚶‍♂🚶‍♂🚶‍♂🚶‍♂🚶‍♂🐠 ")
+	await asyncio.sleep(2)
+	await ult.edit("Reached to Ayushman Guys, making him drink sasta nasha                  🍼🍼🍼🍼🍼🍼🍼🍼🍼🍼")
+	await asyncio.sleep(2)
+	await ult.edit("Something Strange Happening In his Body")
+	await asyncio.sleep(2)
+	await ult.edit("He dreaming while walking")
+	await asyncio.sleep(2)
+	await ult.edit(" 馃鈥嶁檪馃鈥嶁檪馃鈥嶁檧馃鈥嶁檪馃鈥嶁檧馃馃鈥嶁檧馃鈥嶁檧馃馃Ω鈥嶁檪")
+	await asyncio.sleep(2)
+	await ult.edit("Upsos AYUSHMAN billio jaisi harkate krne lga||||||||                                                                                  •••••••••••Made by @MasterK_Rom•••••••••••")
